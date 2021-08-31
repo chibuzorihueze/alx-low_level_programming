0cbfa855c4c96a5de5b52f86d6c4ddc4fb70805a
Chibuzor Ihueze <krisykord@gmail.com>
2021-08-31 11:17:47 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,13 @@
  * print_alphabet - prints alphabet
  */
 void print_alphabet(void)
+  int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
+
+	return 0;
 }
