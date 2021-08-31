fab29f5aae9b6f5dfd6f708ac4d473e54e3ea5e6
Chibuzor Ihueze <krisykord@gmail.com>
2021-08-31 06:45:21 -0700

    putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 /**
 *main - prints Holberton
 *Return: 0
 */
 int main(void)
 {
-     char c[10] = "Holberton";
-     int i;
-
-     for(i = 0; i < 10; i++){
-        _putchar(c[i]);
-	}
-
-     _putchar('\n');
-
-     return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
+
