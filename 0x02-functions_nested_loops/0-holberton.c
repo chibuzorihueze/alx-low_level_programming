b2961c125b976730a0fcc7d62e42bdd8e8880719
Chibuzor Ihueze <krisykord@gmail.com>
2021-08-31 05:05:02 -0700

    print

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,19 @@
-#include "main.h"
+#include "holberton.h"
 /**
 *main - prints Holberton
 *Return: 0
 */
-int main()
+int main(void)
 {
-  if(printf("Holberton")){
-
-  }
-  return 0;
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
