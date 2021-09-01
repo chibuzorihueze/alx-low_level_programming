b2e2e4815d1edc45f16e2398636714898587372c
Chibuzor Ihueze <krisykord@gmail.com>
2021-09-01 09:41:02 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
- * print_alphabet - prints alphabet
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
- 
 {
-  char c = 'a';
-  while (c < = 'z')
-    {
-      _putchar (c)
-    }
-  _putchar('\n');
-  return (0);
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
-  
