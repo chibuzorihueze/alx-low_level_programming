e6e55e3df8c9a25b75977abf1cd9fec574d67177
Chibuzor Ihueze <krisykord@gmail.com>
2021-09-15 05:11:30 -0700

    wildcmp

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,6 +12,4 @@ int _strlen_recursion(char *s);
 int comparator(char *s, int n1, int n2);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int checkLast(char *s, int i);
-int checker(char *s1, char *s2, int a, int b, int wildUsed);
 #endif
