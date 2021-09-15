19d14ae6f78f6721fc07fa1cda78a7b18c17b6d7
Chibuzor Ihueze <krisykord@gmail.com>
2021-09-15 05:02:39 -0700

    100 is palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,4 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int power_operation(int n, int c);
 int is_prime(unsigned int n, unsigned int c);
+int _strlen_recursion(char *s);
+int comparator(char *s, int n1, int n2);
+int is_palindrome(char *s);
 #endif
