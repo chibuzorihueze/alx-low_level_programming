c06d583df6e284431aaafd7907ed7684bcbd12ec
Chibuzor Ihueze <krisykord@gmail.com>
2021-09-17 06:29:10 -0700

    4 add

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,5 +5,5 @@ int _putchar(char c);
 int main(int argc, char const *argv[]);
 int main(int argc, char const *argv[]);
 int main(int argc, char const *argv[]);
-
+int main(int argc, char const *argv[]);
 #endif
