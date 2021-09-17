bf7b45bd288e5a851ff44f9caf3f9ae93d3cf510
Chibuzor Ihueze <krisykord@gmail.com>
2021-09-17 06:33:32 -0700

    change

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -6,4 +6,6 @@ int main(int argc, char const *argv[]);
 int main(int argc, char const *argv[]);
 int main(int argc, char const *argv[]);
 int main(int argc, char const *argv[]);
+int isInteger(const char *s);
+int main(int argc, char const *argv[]);
 #endif
