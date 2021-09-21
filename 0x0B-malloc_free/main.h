91beb63e61801a15cd29eb31a1653b860105fc0d
Chibuzor Ihueze <krisykord@gmail.com>
2021-09-21 06:17:47 -0700

    free grid

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,4 +6,5 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int _strlen(char *s);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 #endif
