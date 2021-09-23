51795c164a00327a57aa6e87732050febd75fc0a
Chibuzor Ihueze <krisykord@gmail.com>
2021-09-22 23:39:57 -0700

    calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c); 
 void *malloc_checked(unsigned int b);
-unsigned int _strlen(char *s);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
 #endif
