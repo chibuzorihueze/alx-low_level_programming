2e6de889ce72fea632b14e8b6605ab640717a4ce
Chibuzor Ihueze <krisykord@gmail.com>
2021-10-14 11:41:19 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,18 +1,41 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * set_bit - gets bit on index `index`
- * @n: decimal number
- * @index: index of the bit
- * Return: 1 (success) or -1(faillure)
+ * _to_power - powers a number b to the p's power
+ * @base: base
+ * @power: power
+ * Return: return b to the power of a
+ */
+
+unsigned long int _to_power(int base, int power)
+{
+	unsigned long int res = 1;
+
+	while (power)
+	{
+		res *= base;
+		power--;
+	}
+	return (res);
+}
+
+/**
+ * set_bit - prints the binary representation of a number
+ * @n: input integer
+ * @index: returns the value of a bit at a given index
+ * Return: 1 for ssucess -1 for failure
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * BYTE_LENGHT))
+	unsigned long int test;
+
+	if (index > sizeof(n) * 8 - 1)
+	{
 		return (-1);
-
-	*n ^= (1 << index);
-
+	}
+	test = _to_power(2, index);
+	*n = *n | test;
 	return (1);
+
 }
