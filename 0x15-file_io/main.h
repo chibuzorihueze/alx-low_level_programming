ca1eb3ea3feeb0b7bacb61d8d3d4f9ca184afdd8
Chibuzor Ihueze <krisykord@gmail.com>
2021-10-18 09:12:37 -0700

    elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,5 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char *argv[]);
 int main(char argc, int *argv[]);
+int main(int ac, char *av[]);
 #endif
