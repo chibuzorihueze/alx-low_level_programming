61ea2c933dceaf0359d276ffb079be27582b94aa
Chibuzor Ihueze <krisykord@gmail.com>
2021-10-18 09:01:36 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,57 +1,37 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file.
- * @filename: name of the input file.
- * @text_content: the NULL terminated string to add at the end of the file.
- *
- * Return: 1 on success and -1 on failure.
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: he NULL terminated string to add at the end of the file
+ * Return: 1 on success and -1 on failure
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, w, i;
+	register int _file, _write, text_len = 0;
 
-	if (!filename)
+	if (!(filename))
+	{
 		return (-1);
-	fd = open(filename, O_APPEND | O_WRONLY);
-	if (fd == -1)
+	}
+	_file = open(filename, O_WRONLY, O_APPEND);
+	if (_file == -1)
+	{
 		return (-1);
+	}
 	if (text_content)
 	{
-		for (i = 0; text_content[i]; i++)
-			;
-		w = write(fd, text_content, i);
-		if (w == -1)
+		while (text_content[text_len])
+		{
+			text_len++;
+		}
+		_write = write(_file, text_content, text_len);
+		if (_write == -1)
+		{
 			return (-1);
+		}
 	}
-	close(fd);
-	return (1);
-}#include "holberton.h"
-
-/**
- * append_text_to_file - appends text at the end of a file.
- * @filename: name of the input file.
- * @text_content: the NULL terminated string to add at the end of the file.
- *
- * Return: 1 on success and -1 on failure.
- */
-int append_text_to_file(const char *filename, char *text_content)
-{
-	int fd, w, i;
-
-	if (!filename)
-		return (-1);
-	fd = open(filename, O_APPEND | O_WRONLY);
-	if (fd == -1)
-		return (-1);
-	if (text_content)
-	{
-		for (i = 0; text_content[i]; i++)
-			;
-		w = write(fd, text_content, i);
-		if (w == -1)
-			return (-1);
-	}
-	close(fd);
+	close(_file);
 	return (1);
 }
